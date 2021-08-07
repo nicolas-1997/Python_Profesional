1039b4ca94bb2e657562eeeb3f96d361f4eac46c
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-08-06 22:24:09 -0300

    we add comments to the code

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,21 @@
+# This code is for practicing static typing
+
 def is_palindrome(string: str):
-    string = string.replace(" ", "").lower()
+    string = string.replace(" ", "").lower() #we clean the word and save it in a variable
     
-    if string == string[::-1]:
-        print("This is palindrome!!", string,"=", string[::-1])
+    #we compare the word with same but other way around
+    if string == string[::-1]:  #[::-1 serves to turn]
+        print("This is palindrome!!", string,"=", string[::-1]) 
     else:
         print("Not is a palindrome!!", string,"=", string[::-1])
 
 
 def run():
-    palindrome = input("Enter a word: ")
+    palindrome = input("Enter a word: ") #we ask the user for a word
 
-    is_palindrome(palindrome)
+    is_palindrome(palindrome) #we pass this word as a parameter for the function
 
 if __name__=="__main__":
     run()
+
+
